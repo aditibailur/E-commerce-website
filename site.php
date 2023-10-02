18036f2bf29632cecfe1e7c9e04c43fc8aa34533
aditibailur <83756035+aditibailur@users.noreply.github.com>
2023-10-02 20:50:59 +0530

    Add files via upload

---
+++ site.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $username ="Aditi";
+    $Password ="au";
+    include "login.html" ?>
+  </body>
+</html>
